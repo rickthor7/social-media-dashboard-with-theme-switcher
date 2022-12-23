f28430eb5baef5fc7d2deebede3a0fcc4d031185
nerodevo <>
2022-12-23 15:15:13 -0600

    Initial solution

---
+++ js/default.js
@@ -0,0 +1,21 @@
+(() => {
+	function setTheme(themeName) {
+		localStorage.setItem('theme', themeName);
+		document.documentElement.className = themeName;
+	}
+
+	function toggleTheme() {
+		if (localStorage.getItem('theme') === 'theme-dark') {
+			setTheme('theme-light');
+		} else {
+			setTheme('theme-dark');
+		}
+	}
+
+	if (localStorage.getItem('theme') === 'theme-dark') {
+		setTheme('theme-dark');
+	} else {
+		setTheme('theme-light');
+	}
+	document.getElementById('theme-checkbox').addEventListener('change', toggleTheme, false);
+})();
